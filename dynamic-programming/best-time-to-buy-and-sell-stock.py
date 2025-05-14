e7cdd4e40be3e59a28c4407f4dd2a6892caebd6e
Wei <83650106+wei892@users.noreply.github.com>
2025-05-13 21:07:39 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        maxP = 0
+
+        while right < len(prices):
+            print(left, right)
+            if prices[right] < prices[left]:
+                left = right
+                right += 1
+            profit = prices[right] - prices[left]
+            maxP = max(maxP, profit)
+            right += 1
+        
+        return maxP
